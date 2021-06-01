ec14cf1deb12588f43322a956b7ab6b55468f165
Sagnik Bhattacharya <sagnikbhattacharya1506@gmail.com>
2021-06-01 18:19:18 +0530

    Card offers

--- lib/main.dart
+++ lib/main.dart
@@ -1,68 +1,81 @@
-import 'package:all_tests/view_model.dart';
+import 'dart:math';
+import 'package:aligned_dialog/aligned_dialog.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
-import 'package:hooks_riverpod/hooks_riverpod.dart';
 
-void main() => runApp(ProviderScope(child: MyApp()));
+void main() {
+  runApp(MyApp());
+}
 
-class MyApp extends HookWidget {
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    var state=useProvider(viewModel).state;
-    var views=useProvider(viewModel);
     return MaterialApp(
-      //theme: ThemeData.dark(),
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text("Testing background deminishing"),
-        ),
-        body: ListView(
-          children: [
-            Padding(
-              padding: const EdgeInsets.all(30.0),
-              child: MaterialButton(
-                color: state?Colors.green:Colors.blueAccent,
-                onPressed: () {
-                  views.changeState();
-                },
-                child: Text("This is a test chat"),
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(30.0),
-              child: MaterialButton(
-                color: Colors.green,
-                onPressed: () {},
-                child: Text("This is a test chat"),
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(30.0),
-              child: MaterialButton(
-                color: Colors.green,
-                onPressed: () {},
-                child: Text("This is a test chat"),
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(30.0),
-              child: MaterialButton(
-                color: Colors.green,
-                onPressed: () {},
-                child: Text("This is a test chat"),
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(30.0),
-              child: MaterialButton(
-                color: Colors.green,
-                onPressed: () {},
-                child: Text("This is a test chat"),
-              ),
-            ),
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
 
-          ],
+class MyHomePage extends HookWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: SafeArea(
+           child: Scaffold(
+           body: Expanded(
+            child: Stack(
+              children: [
+                Container(
+                  height: 248,
+                  width: double.infinity,
+                  decoration: BoxDecoration(
+                    image: DecorationImage(
+                      fit: BoxFit.fill,
+                      image: NetworkImage(
+                        'https://github.com/flutter/plugins/raw/master/packages/video_player/video_player/doc/demo_ipod.gif?raw=true',
+                      ),
+                    ),
+                    borderRadius: BorderRadius.all(Radius.circular(8.0)),
+                    color: Colors.redAccent,
+                  ),
+                ),
+                Positioned(
+                  child: Container(
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.all(Radius.circular(16.0)),
+                      color: Color(0XFFFFFFFF),
+                    ),
+                    child: Text(
+                      "near offers",
+                      style: TextStyle(
+                        color: Color(0XFF4B6E82),
+                        fontSize: 14,
+                        fontWeight: FontWeight.w400,
+                      ),
+                      maxLines: 1,
+                      overflow: TextOverflow.ellipsis,
+                    ),
+                    padding: EdgeInsets.fromLTRB(8, 4, 8, 4),
+                  ),
+                  bottom: 10,
+                  left: 5,
+                ),
+                Positioned(
+                  child: IconButton(icon: Icon(Icons.inbox), onPressed: () {}),
+                  bottom: -1,
+                  right: -5,
+                ),
+              ],
+            ),
+          ),
         ),
       ),
     );
--- lib/view_model.dart
+++ lib/view_model.dart
@@ -6,10 +6,31 @@ final viewModel = ChangeNotifierProvider.autoDispose<ViewModel>((ref) {
 });
 
 class ViewModel extends ChangeNotifier {
-  bool state = false;
+  bool state = true;
+  Widget con=Container();
 
   changeState() {
     state = !state;
+    notifyListeners();
+  }
+  conChange(bool val){
+    con=
+        val?Column(
+      children: [
+        Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Text("Hey"),
+        ),
+        Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Text("Hey"),
+        ),
+        Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Text("Hey"),
+        ),
+      ],
+    ):Container();
     notifyListeners();
   }
 }
